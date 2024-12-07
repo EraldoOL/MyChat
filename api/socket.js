9512572747109c741a5e913fb70e3a18df20a172
EraldoOL <eo80797@gmail.com>
2024-12-07 15:06:18 -0300

    update socket.js

--- api/socket.js
+++ api/socket.js
@@ -1,31 +1,36 @@
-const { Server } = require('socket.io');
+const express = require('express');
+const socketIo = require('socket.io');
+const { Server } = require('http');
 
-let io;
+const app = express();
+const server = new Server(app); // Criação do servidor HTTP
+const io = socketIo(server); // Inicializando o Socket.IO com o servidor HTTP
 
-module.exports = (req, res) => {
-  if (!res.socket.server.io) {
-    console.log('Iniciando Socket.IO...');
-    io = new Server(res.socket.server);
+app.use(express.static('public')); // Servindo os arquivos estáticos
 
-    io.on('connection', (socket) => {
-      console.log('Usuário conectado: ' + socket.id);
+// Configuração do socket.io
+io.on('connection', (socket) => {
+  console.log('Usuário conectado: ' + socket.id);
 
-      // Lida com mensagens enviadas pelos usuários
-      socket.on('chatMessage', (msg) => {
-        io.emit('message', msg); // Emite a mensagem para todos os clientes conectados
-      });
+  socket.on('audio-stream', (audioBlob) => {
+    socket.broadcast.emit('audio-stream', audioBlob);
+  });
 
-      socket.on('disconnect', () => {
-        console.log('Usuário desconectado: ' + socket.id);
-      });
-    });
+  socket.on('chatMessage', (msg) => {
+    io.emit('message', msg);
+  });
 
-    res.socket.server.io = io; // Associar o servidor ao objeto `res`
-  }
-  res.end(); // Finaliza a resposta
-};
+  socket.on('disconnect', () => {
+    console.log('Usuário desconectado: ' + socket.id);
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+// Exporta o servidor para que a Vercel/Render possa usá-lo
+module.exports = (req, res) => {
+  server.emit('request', req, res);
+};
