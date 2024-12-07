b3a2476562c8ca15ba017ddfeac83798d42aa89d
EraldoOL <eo80797@gmail.com>
2024-12-07 14:59:19 -0300

    update

--- api/socket.js
+++ api/socket.js
@@ -24,3 +24,8 @@ module.exports = (req, res) => {
   }
   res.end(); // Finaliza a resposta
 };
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
