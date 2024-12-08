f0c537ae201b5a85cc9f28d1d2c3115991452156
EraldoOL <eo80797@gmail.com>
2024-12-08 18:09:02 -0300

    msg1

--- api/socket.js
+++ api/socket.js
@@ -1,30 +1,6 @@
 const express = require('express');
 const socketIo = require('socket.io');
 const { Server } = require('http');
-const mongoose = require('mongoose');
-
-const connectionString = 
-'mongodb+srv://Pionne:eraldo@cluster0.rw9vw.mongodb.net/Messenger?retryWrites=true&w=majority&appName=Cluster0';
-
-mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log('Conectado ao MongoDB com sucesso!');
-})
-.catch(err => {
-  console.error('Erro ao conectar ao MongoDB:', err);
-});
-
-// Definição do esquema e modelo de mensagem
-const messageSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  message: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now },
-});
-const Message = mongoose.model('Message', messageSchema)
-;
 
 const app = express();
 const server = new Server(app); // Criação do servidor HTTP
