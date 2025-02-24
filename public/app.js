778ae52dc65ff16c4e369bae2c3a8da607cdd4ab
EraldoOL <eo80797@gmail.com>
2025-02-24 15:37:21 -0300

    update app.js

--- public/app.js
+++ public/app.js
@@ -7,7 +7,7 @@ function addMessage(message) {
     document.getElementById('messages').appendChild(li);
 }
 
-// Recebe mensagem do servidor e ex
+// Recebe mensagem do servidor e e
 socket.on('message', (msg) => {
     addMessage(msg);
 });
