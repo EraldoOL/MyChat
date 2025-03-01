9a9f42572c568b3520788d7786e3e490f3cf9e6f
EraldoOL <eo80797@gmail.com>
2025-03-01 17:20:39 -0300

    update app.js

--- public/app.js
+++ public/app.js
@@ -7,7 +7,7 @@ function addMessage(message) {
     document.getElementById('messages').appendChild(li);
 }
 
-// Recebe mensag
+// Recebe mensa
 socket.on('message', (msg) => {
     addMessage(msg);
 });
