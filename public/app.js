ac018eab601521cf3b4b69ccbb449c41d225b07a
EraldoOL <eo80797@gmail.com>
2025-03-02 12:20:25 -0300

    update app.js

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 // Criação da conexão com o servidor Socket.IO
-const socket = io(); // Inicializa
+const socket = io(); // Inicializ
 
 function addMessage(message) {
     const li = document.createElement('li');
