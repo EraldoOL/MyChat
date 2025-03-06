2eb2e737452b6fc6453bc8af8f7ffbb17384ed9c
EraldoOL <eo80797@gmail.com>
2025-03-06 16:45:42 -0300

    update app.js

--- public/app.js
+++ public/app.js
@@ -10,7 +10,7 @@ function addMessage(message) {
 socket.on('message', (msg) => {
     addMessage(msg);
 });
-// No lado do cliente para receber o stre
+// No lado do cliente para receber o str
 socket.on('audio-stream', (audioBlob) => {
   const audioUrl = URL.createObjectURL(audioBlob);
   const audio = new Audio(audioUrl);
