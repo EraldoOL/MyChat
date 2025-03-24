669e65eeec6029765982341a356a6263fe1aa404
EraldoOL <eo80797@gmail.com>
2025-03-24 13:59:45 -0300

    update socket.js

--- api/socket.js
+++ api/socket.js
@@ -4,7 +4,7 @@ const { Server } = require('http');
 
 const app = express();
 const server = new Server(app); 
-const io = socketIo(server); // Inicializando o Socket.IO com o servidor
+const io = socketIo(server); // Inicializando o Socket.IO com o servido
 
 app.use(express.static('public')); // Servindo os arquivos estáticos
 app.use('/imgs', express.static('imgs'));
