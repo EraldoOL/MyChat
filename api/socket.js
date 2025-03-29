7fa424f890dea0afac2de883338f1c946509ce1b
EraldoOL <eo80797@gmail.com>
2025-03-29 15:55:06 -0300

    update socket.js

--- api/socket.js
+++ api/socket.js
@@ -6,7 +6,7 @@ const app = express();
 const server = new Server(app); 
 const io = socketIo(server); 
 
-app.use(express.static('public')); // Servindo os arquivos est
+app.use(express.static('public')); // Servindo os arquivos e
 app.use('/imgs', express.static('imgs'));
 
 // Configuração do socket.io
