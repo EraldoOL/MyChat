2c6c339151c70d16079d0ea95a4bfd93df82525d
EraldoOL <eo80797@gmail.com>
2025-03-31 15:58:20 -0300

    update socket.js

--- api/socket.js
+++ api/socket.js
@@ -20,7 +20,7 @@ io.on('connection', (socket) => {
   
   socket.on('chatMessage', (msg) => {
   if (typeof msg === 'object' && msg.username && msg.message) {
-    // Formata a mensagem no forma
+    // Formata a mensagem no form
     const formattedMessage = `${msg.username.toUpperCase()}: ${msg.message}`;
     io.emit('message', formattedMessage);
   } else {
