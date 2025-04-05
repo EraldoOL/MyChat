a83f94dc59687320c22db55838be1e4d977a770a
EraldoOL <eo80797@gmail.com>
2025-04-05 16:00:20 -0300

    update socket.js

--- api/socket.js
+++ api/socket.js
@@ -37,7 +37,7 @@ server.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
 
-// Exporta o servidor para que a Vercel/Render poss
+// Exporta o servidor para que a Vercel/Render pos
 module.exports = (req, res) => {
   server.emit('request', req, res);
 };
