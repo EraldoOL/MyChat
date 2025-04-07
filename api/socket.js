a80f2e1fb531c044c491373b2759dfbf586e8c69
EraldoOL <eo80797@gmail.com>
2025-04-07 16:42:39 -0300

    update socket.js

--- api/socket.js
+++ api/socket.js
@@ -37,7 +37,7 @@ server.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
 
-// Exporta o servidor para que a Ver
+// Exporta o servidor para que a Ve
 module.exports = (req, res) => {
   server.emit('request', req, res);
 };
