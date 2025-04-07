109d7318c34890e858c45439baa1abddcc84f568
EraldoOL <eo80797@gmail.com>
2025-04-07 16:42:15 -0300

    update socket.js

--- api/socket.js
+++ api/socket.js
@@ -37,7 +37,7 @@ server.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
 
-// Exporta o servidor para que a Vercel
+// Exporta o servidor para que a Verce
 module.exports = (req, res) => {
   server.emit('request', req, res);
 };
