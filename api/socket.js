6485f79cbf3fa4167cf1406c41ec46f1e862058b
EraldoOL <eo80797@gmail.com>
2025-04-15 17:08:55 -0300

    update socket.js

--- api/socket.js
+++ api/socket.js
@@ -9,7 +9,7 @@ const io = socketIo(server);
 app.use(express.static('public')); 
 app.use('/imgs', express.static('imgs'));
 
-// Configu
+// Config
 io.on('connection', (socket) => {
   console.log('Usuário conectado: ' + socket.id);
 
