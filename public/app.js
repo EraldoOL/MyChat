6c61469bfd16be7431f6e0ebd20257ebb7f20b5e
EraldoOL <eo80797@gmail.com>
2025-05-10 17:25:08 -0300

    update app.js

--- public/app.js
+++ public/app.js
@@ -17,7 +17,7 @@ socket.on('audio-stream', (audioBlob) => {
   audio.play();
 });
 
-// Enviar mensagem quando o botão for cl
+// Enviar mensagem quando o botão for c
 document.getElementById('send-btn').addEventListener('click', () => {
     const message = document.getElementById('chat-input').value;
     if (message.trim()) {
