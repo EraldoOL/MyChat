9332711cc3de45551941ce83a70760f2806fe610
EraldoOL <eo80797@gmail.com>
2025-05-15 16:34:52 -0300

    update app.js

--- public/app.js
+++ public/app.js
@@ -21,7 +21,7 @@ socket.on('audio-stream', (audioBlob) => {
 document.getElementById('send-btn').addEventListener('click', () => {
     const message = document.getElementById('chat-input').value;
     if (message.trim()) {
-        socket.emit('chatMessage', message);  // Envia a mensagem para o ser
+        socket.emit('chatMessage', message);  // Envia a mensagem para o se
         document.getElementById('chat-input').value = '';  // Limpa o campo de input
     }
 });
