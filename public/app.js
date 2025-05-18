bece0f95c0c0f369dd156adc4c383ca98fb579c8
EraldoOL <eo80797@gmail.com>
2025-05-18 16:58:00 -0300

    update app.js

--- public/app.js
+++ public/app.js
@@ -21,7 +21,7 @@ socket.on('audio-stream', (audioBlob) => {
 document.getElementById('send-btn').addEventListener('click', () => {
     const message = document.getElementById('chat-input').value;
     if (message.trim()) {
-        socket.emit('chatMessage', message);  /
+        socket.emit('chatMessage', message);  
         document.getElementById('chat-input').value = '';  // Limpa o campo de input
     }
 });
