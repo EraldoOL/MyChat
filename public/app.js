611b82fed52075cf47b58f08116274ba59f5bcbe
EraldoOL <eo80797@gmail.com>
2025-05-20 17:47:11 -0300

    update app.js

--- public/app.js
+++ public/app.js
@@ -29,7 +29,7 @@ document.getElementById('send-btn').addEventListener('click', () => {
 // Enviar mensagem quando pressionar Enter
 document.getElementById('chat-input').addEventListener('keypress', (event) => {
     if (event.key === 'Enter' && document.getElementById('chat-input').value.trim()) {
-        socket.emit('chatMessage', document.getElementById('chat-input').value);  // Envia a mensagem para o servidor
+        socket.emit('chatMessage', document.getElementById('chat-input').value);  // Envia a mensagem para o servido
         document.getElementById('chat-input').value = '';  // Limpa o campo de input
     }
 });
