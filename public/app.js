5bd0f9a6e98ddfecb3b4af5b2490a12d04a9ff7f
EraldoOL <eo80797@gmail.com>
2025-05-31 16:55:19 -0300

    update app.js

--- public/app.js
+++ public/app.js
@@ -34,7 +34,7 @@ document.getElementById('chat-input').addEventListener('keypress', (event) => {
     }
 });
 
-// Função para iniciar o reconh
+// Função para iniciar o recon
 const micButton = document.getElementById('microphone-btn');
 
 if ('webkitSpeechRecognition' in window) {
