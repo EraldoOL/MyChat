4b65986eccc3238c9340c6418d4fb92e8278c71c
EraldoOL <eo80797@gmail.com>
2025-05-31 16:56:26 -0300

    update app.js

--- public/app.js
+++ public/app.js
@@ -34,7 +34,7 @@ document.getElementById('chat-input').addEventListener('keypress', (event) => {
     }
 });
 
-// Função para iniciar
+// Função para inicia
 const micButton = document.getElementById('microphone-btn');
 
 if ('webkitSpeechRecognition' in window) {
