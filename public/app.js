56857d2cc9d4b7eeccb90e22db9cb1a58c576d88
EraldoOL <eo80797@gmail.com>
2025-06-02 17:35:59 -0300

    update app.js

--- public/app.js
+++ public/app.js
@@ -54,7 +54,7 @@ if ('webkitSpeechRecognition' in window) {
         }
         document.getElementById('chat-input').value = transcript;
 
-        // Enviar o texto transcrito como 
+        // Enviar o texto transcrito como
         socket.emit('chatMessage', transcript);
     };
 
