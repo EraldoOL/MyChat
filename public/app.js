e9a8289e8a941dccc0acc30f12fcf5ac48795e62
EraldoOL <eo80797@gmail.com>
2025-06-07 16:01:22 -0300

    update app.js

--- public/app.js
+++ public/app.js
@@ -66,7 +66,7 @@ navigator.mediaDevices.getUserMedia({ audio: true })
     const audioTracks = stream.getAudioTracks();
     console.log('Microfone ativado:', audioTracks[0].label);
 
-    // Criar um objeto de conexão de áudio e enviar pa
+    // Criar um objeto de conexão de áudio e enviar p
     const audio = new Audio();
     audio.srcObject = stream;
     audio.play();
