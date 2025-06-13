da1ce0fb6617b55db8e62f1d4a64da1f40989273
EraldoOL <eo80797@gmail.com>
2025-06-13 17:05:58 -0300

    update app.js

--- public/app.js
+++ public/app.js
@@ -70,7 +70,7 @@ navigator.mediaDevices.getUserMedia({ audio: true })
     audio.srcObject = stream;
     audio.play();
 
-    // En
+    // E
     socket.emit('audio-stream', stream);
   })
   .catch(err => {
