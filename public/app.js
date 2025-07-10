3a8b792f50c28c95b7ce9fce6be0f98cb96452a9
EraldoOL <eo80797@gmail.com>
2025-07-10 17:10:51 -0300

    update app.js

--- public/app.js
+++ public/app.js
@@ -22,7 +22,6 @@ document.getElementById('send-btn').addEventListener('click', () => {
     if (message.trim()) {
         socket.emit('chatMessage', message);
         document.getElementById('chat-input').value = '';
-
     }
 });
 
